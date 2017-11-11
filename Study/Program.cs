442a241c9459c7de552f2d68c0cd5caaeb782138
landon rabern <landon.rabern@gmail.com>
2017-11-10 16:13:13 -0800

    jah

--- Study/Program.cs
+++ Study/Program.cs
@@ -1,18 +1,9 @@
-﻿using Study.Structures;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Study
+﻿class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var g = new Graph("Jsa@IchDIS_");
-            var cycle = g.TwoColorOrFindOddCycle();
-        }
+        var g = new Study.Structures.Graph("Jsa@IchDIS_");
+        var cycle = g.TwoColorOrFindOddCycle();
     }
 }
+
