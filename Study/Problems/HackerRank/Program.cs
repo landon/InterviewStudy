848767cbf43c38f943b77cfa1fc651d12c2a05fa
landon rabern <landon.rabern@gmail.com>
2017-11-23 13:57:25 -0800

    m

--- Study/Problems/HackerRank/Program.cs
+++ Study/Problems/HackerRank/Program.cs
@@ -8,36 +8,16 @@ namespace HackerRank
 {
     class Program
     {
-        public static void Main()
+        static int maxValue(string t)
         {
-            var dd = DecodeString("3[a]2[bc]");
-            Console.WriteLine(dd);
-            Console.ReadKey();
+            return 0;
         }
 
-        public static string DecodeString(string s)
+        static void Main(String[] args)
         {
-            var lastUp = -1;
-            for (int i = 0; i < s.Length; i++)
-            {
-                if (s[i] == '[')
-                    lastUp = i;
-                if (s[i] == ']')
-                {
-                    int j = lastUp - 1;
-                    while (j >= 0 && char.IsDigit(s[j]))
-                        j--;
-                    j++;
-                    var count = int.Parse(s.Substring(j, lastUp - j));
-                    var q = s.Substring(lastUp + 1, i - 1 - lastUp);
-                    var x = "";
-                    for (int w = 0; w < count; w++)
-                        x += q;
-                    return DecodeString(s.Substring(0, j) + x + s.Substring(i + 1));
-                }
-            }
-
-            return s;
+            string t = Console.ReadLine();
+            int result = maxValue(t);
+            Console.WriteLine(result);
         }
     }
 }
