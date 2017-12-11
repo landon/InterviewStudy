c3998ebe4045b4de66a0b5143cd708519dda2b62
landon rabern <landon.rabern@gmail.com>
2017-12-11 14:37:49 -0800

    m

--- Study/Problems/HackerRank/Program.cs
+++ Study/Problems/HackerRank/Program.cs
@@ -8,94 +8,43 @@ namespace HackerRank
 {
     class Program
     {
+        static int anagram(string s)
+        {
+            if (s.Length % 2 == 1)
+                return -1;
+
+            var c1 = new Dictionary<char, int>();
+            var c2 = new Dictionary<char, int>();
+
+            foreach (var c in s)
+            {
+                c1[c] = 0;
+                c2[c] = 0;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i < s.Length / 2)
+                    c1[s[i]]++;
+                else
+                    c2[s[i]]++;
+            }
+
+            var d = s.Distinct().Select(c => c2[c] - c1[c]).ToList();
+            return d.Where(aa => aa > 0).Sum();
+        }
+
         static void Main(String[] args)
         {
-            var mnr = new int[] { 210, 202, 7865 };//Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToList();
-            var m = mnr[0];
-            var n = mnr[1];
-            var r = mnr[2];
-            var d = new int[m, n];
-            var periods = ComputePeriods(m, n);
-
-            for (int i = 0; i < m; i++)
+            int q = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < q; a0++)
             {
-                var j = 0;
-                foreach (var x in Console.ReadLine().Split(' ').Select(x => int.Parse(x)))
-                {
-                    var q = ToOuterRing(i, j, m, n);
-                    var p = periods[q];
-                    var tup = Destination(i, j, m, n, r % p);
-                    d[tup.Item1, tup.Item2] = x;
-                    j++;
-                }
+                string s = Console.ReadLine();
+                int result = anagram(s);
+                Console.WriteLine(result);
             }
-
-            for (int i = 0; i < m; i++)
-                Console.WriteLine(string.Join(" ", Enumerable.Range(0, n).Select(j => d[i, j])));
 
             Console.ReadKey();
-        }
-
-        static List<int> ComputePeriods(int m, int n)
-        {
-            var periods = new List<int>();
-            for (int k = 0; k <= Math.Min(m, n); k++)
-            {
-                var p = 2 * (m + n - 4 * k) - 4;
-                if (p <= 0)
-                    break;
-                periods.Add(p);  
-            }
-            return periods;
-        }
-
-        static Tuple<int, int> Destination(int i, int j, int m, int n, int r)
-        {
-            if (r == 0)
-                return new Tuple<int, int>(i, j);
-            var tup = Destination(i, j, m, n, r - 1);
-            var answer = Destination(tup.Item1, tup.Item2, m, n);
-
-            return answer;
-        }
-
-        static Tuple<int, int> Destination(int i, int j, int m, int n)
-        {
-            Tuple<int, int> answer = null;
-            var q = ToOuterRing(i, j, m, n);
-            var left = ToOuterRing(i, j - 1, m, n);
-            var right = ToOuterRing(i, j + 1, m, n);
-            var up = ToOuterRing(i - 1, j, m, n);
-            var down = ToOuterRing(i + 1, j, m, n);
-
-            if (up < q)  // on topmost part of our cycle
-            {
-                if (left == q) // can freely move left, so move left
-                    answer = new Tuple<int, int>(i, j - 1);
-                else
-                    answer = new Tuple<int, int>(i + 1, j); // otherwise must be top left corner, move down
-            }
-            else if (down < q) // on bottommost part of our cycle
-            {
-                if (right == q) // can freely move right, so move right
-                    answer = new Tuple<int, int>(i, j + 1);
-                else
-                    answer = new Tuple<int, int>(i - 1, j); // otherwise must be bottom right corner, move up
-            }
-            else // inside the left or right side
-            {
-                if (left < q)  // if left, move down
-                    answer =  new Tuple<int, int>(i + 1, j);
-                else
-                    answer = new Tuple<int, int>(i - 1, j); // otherwise in right side, move up
-            }
-
-            return answer;
-        }
-
-        static int ToOuterRing(int i, int j, int m, int n)
-        {
-            return Math.Min(Math.Min(i, j), Math.Min(m - 1 - i, n - 1 - j));
         }
     }
 }
---
+++ Study/Utility/Strings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Utility
+{
+    public static class Strings
+    {
+        public static int CountSubstring(string s, string ss)
+        {
+            var count = 0;
+            var i = 0;
+            while (i < s.Length)
+            {
+                i = s.IndexOf(ss, i);
+                if (i < 0)
+                    return count;
+                count++;
+                i++;
+            }
+
+            return count;
+        }
+
+        public static bool IsPalindrome(string s)
+        {
+            return Enumerable.Range(0, s.Length / 2).All(i => s[i] == s[s.Length - 1 - i]);
+        }
+    }
+}
